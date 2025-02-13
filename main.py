687cf7610894962b85744fc4f7e2bfa28a1af81e
austin <austinmpask@gmail.com>
2025-02-12 21:40:27 -0800

    add tests, refactor

--- main.py
+++ main.py
@@ -1,100 +1,7 @@
-import sys
 from getpass import getpass
-from typing import Iterable, TypedDict
 
-
-# Define option types for autocomplete etc.
-class PromptOptions(TypedDict, total=False):
-
-    # Behavior
-    loop: bool
-    isPassword: bool
-    clearAfterResponse: bool
-
-    # Validators
-    alphaOnly: bool
-    numOnly: bool
-    alphaNumOnly: bool
-    floatOnly: bool
-    notEmpty: bool
-
-    # Formatting - PROMPT
-    capPrompt: bool
-    titlePrompt: bool
-    carat: bool
-    caratStr: str
-    color: str
-    suffix: bool
-    suffixStr: str
-
-    # Formatting - ANSWER
-    capAnswer: bool
-    titleAnswer: bool
-    castAnswer: bool
-    trueStrs: Iterable[str]
-    falseStrs: Iterable[str]
-
-
-def isfloat(string: str) -> bool:
-
-    # Check for decimal
-    if "." not in string:
-        return False
-
-    # Try to cast
-    try:
-        float(string)
-        return True
-    except ValueError:
-        return False
-
-
-def clrLn():
-    """Clears one line from std. out"""
-    sys.stdout.write("\033[F")  # Move cursor up one line
-    sys.stdout.write("\033[K")  # Clear to end of line
-
-
-def validate(a: str, options: dict) -> bool:
-
-    # Map options to validations
-    validators = [
-        ("alphaOnly", str.isalpha),
-        ("numOnly", str.isnumeric),
-        ("floatOnly", isfloat),
-        ("alphaNumOnly", str.isalnum),
-        ("notEmpty", lambda x: x != ""),
-    ]
-
-    # Return false if any validator did not pass
-    for o, validator in validators:
-        if options[o]:
-            if not validator(a):
-                return False
-
-    # Validations passed
-    return True
-
-
-def inferType(
-    a: str, trueStrs: Iterable[str], falseStrs: Iterable[str]
-) -> bool | int | float | str | None:
-    if a.isnumeric():
-        return int(a)
-
-    if isfloat(a):
-        return float(a)
-
-    if a.lower() in trueStrs:
-        return True
-
-    if a.lower() in falseStrs:
-        return False
-
-    if a == "":
-        return None
-
-    return a
+from config import COLORS, PromptOptions
+from utils import clrLn, inferType, validate
 
 
 def prompt(
@@ -121,28 +28,18 @@ def prompt(
     clearAfterResponse=False,
     trueStrs=("true", "yes", "y"),
     falseStrs=("false", "no", "n"),
+    pwFailMsg="Invalid password",
+    validators=[],
 ):
 
-    # Forward option kwargs
+    # Create a dict out of kwarg options
     dOptions: PromptOptions = {
         k: v for k, v in locals().items() if k not in ("prompt", "options")
     }
 
-    # Update from options obj if present
+    # If user supplied options via dict, update with those values instead
     if options:
         dOptions.update(options)
-
-    colors = {
-        "black": "\033[30m",
-        "red": "\033[31m",
-        "green": "\033[32m",
-        "yellow": "\033[33m",
-        "blue": "\033[34m",
-        "magenta": "\033[35m",
-        "cyan": "\033[36m",
-        "white": "\033[37m",
-        "reset": "\033[0m",
-    }
 
     # Map prompt formatting options to functions
     promptFormatters = [
@@ -152,37 +49,37 @@ def prompt(
         ("suffix", lambda x: f"{x}{suffixStr}"),
         (
             "color",
-            lambda x: f"{colors.get(color, colors["reset"])}{x}{colors["reset"]}",
+            lambda x: f"{COLORS.get(color, COLORS["reset"])}{x}{COLORS["reset"]}",
         ),
     ]
 
-    # Format prompt
+    # Format prompt based on selected formatting options
     for o, fn in promptFormatters:
         if dOptions[o]:
             prompt = fn(prompt)
 
-    # Pose prompt
+    # Pose prompt to user
     while True:
+
+        # Obscure text if input is for password
         if dOptions["isPassword"]:
             a = getpass(prompt)
         else:
             a = input(prompt)
 
+        # Exit the loop if validations pass, or if looping is disabled
         if validate(a, dOptions) or not dOptions["loop"]:
+
+            # Clear line following end of lifecycle if chosen
             if dOptions["clearAfterResponse"]:
                 clrLn()
             break
-        # Clear line if prompt will be re-posed
+
+        # Clear line if prompt will be posed again
         if dOptions["isPassword"]:
-            print("Invalid password")
+            print(pwFailMsg)
         else:
             clrLn()
-
-    # Map answer formatting options to functions
-    ansFormatters = [
-        ("capAnswer", str.capitalize),
-        ("titleAnswer", str.title),
-    ]
 
     # Type cast answer
     if dOptions["castAnswer"]:
@@ -190,13 +87,16 @@ def prompt(
 
     # Format answer if it is still a string
     if isinstance(a, str):
+
+        # Map answer formatting options to functions
+        ansFormatters = [
+            ("capAnswer", str.capitalize),
+            ("titleAnswer", str.title),
+        ]
+
+        # Format according to choices
         for o, fn in ansFormatters:
             if dOptions[o]:
                 a = fn(a)
 
     return a
-
-
-ans = prompt("hello", alphaOnly=True)
-
-print(ans)
